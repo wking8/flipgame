37fc59a3ca78a04e82c6be4ee7940c64e3bf9203
Wyatt King <wkmusic8@gmail.com>
2019-12-03 11:20:17 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1>Flipgame</h1>
+      </div>
+      <div>
+        <h2>Find the matching card</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
